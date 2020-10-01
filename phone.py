fed483ad99f40ee8b1c42bb494d44cd4edea1a24
Anahis259 <anahisbetsabe.1018@gmail.com>
2020-10-01 15:37:43 -0300

    Changes in Get_string,Get_phone, and Get_number

--- phone.py
+++ phone.py
@@ -6,18 +6,23 @@ class Phone:
         self.phone_letters = phone_letters
 
     def validate_phone(self):
-        """
-          Return false if a phone is not valid.
-          A valid phone number should match these characteristics:
-          - It should start with +.
-          - It shouldn't be smaller than 15 characters.
-          - Aside from the + no other special character should be permitted (i.e: #,@,%,etc).
-        """
+        #"""
+          #Return false if a phone is not valid.
+          #A valid phone number should match these characteristics:
+          #- It should start with +.
+          #- It shouldn't be smaller than 15 characters.
+          #- Aside from the + no other special character should be permitted (i.e: #,@,%,etc).
+        #"""
+        phone_doesnt_start_with_plus = Phone("+13463335555")
+        phone_greater_than_15_characters = Phone("+13463335555321412342134")
+       	phone_with_special_characters = Phone("!~#$%^&*@|}{")
+	
         if self.phone_letters:
             if self.phone_letters[0] == "+" and len(self.phone_letters) < 15:
                 return True
         return False
-
+    
+    
     def get_string(self):
         """Returns the corresponding phone's string shape of the using only |, _, -, /, \ """
         phone_digit_shape_lists = []
@@ -37,8 +42,12 @@ class Phone:
         return phone_digit_shape
 
     def get_number(self, symbol):
-        """
-          Should convert letter to digit (see image in the README.md)
-          clue 1: use a dictionary.
-        """
-        pass
+        phone_keyboard_mapper ={'a':1, 'b':1,'c':1,'d':2,'e':2,'f':2,'g':3,'h':3,'i':3,'j':4,'k':4,'l':4,'m':5,'n':5,'o':5,'p':6,'q':6,'r':6,'s':7,'t':7,'u':7,'v':8,'w':8,'x':8,'y':9,'z':9, 
+
+        }
+        return (phone_keyboard_mapper.get(symbol.lower(),symbol))
+
+        #"""
+          #Should convert letter to digit (see image in the README.md)
+          #clue 1: use a dictionary.
+        #"""
--- symbol_factory.py
+++ symbol_factory.py
@@ -1,9 +1,13 @@
+from helper import my_dict
 class SymbolFactory:
     def get_string(self, single_simbol):
-        """
-          Returns the corresponding symbol's string shape using only |, _, -, /, \,
-          Look at the example output in README.md.
+      return my_dict[single_simbol]
 
-          clue 1: use a dictionary to match the hardcode string with the corresponding symbol.
-        """
-        pass
+
+        #"""
+          #Returns the corresponding symbol's string shape using only |, _, -, /, \,
+          #Look at the example output in README.md.
+
+          #clue 1: use a dictionary to match the hardcode string with the corresponding symbol.
+        #"""
+        #pass
